d1da3596f96899dc8870e0171c605fa16d8efbaa
jijo406 <jjoseph8@buffalo.edu>
2016-11-08 22:56:56 -0500

    Commit 2

---
+++ TillDeath/TillDeath/FirstScene.swift
@@ -0,0 +1,13 @@
+//
+//  FirstScene.swift
+//  TillDeath
+//
+//  Created by Jobin Joseph on 11/8/16.
+//  Copyright © 2016 NaanBoys. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class FirstScene: UIViewController {
+    
+}
